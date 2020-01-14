622cec60ed5e4a555c8e19e4ae6d8e735b67d8e4
Unknown <gabriele.aldeghi.27@gmail.com>
2020-01-14 10:12:31 +0100

    Refactored static image paths inside ImageWrapper

--- mobile_application/lib/widgets/general/image_wrapper.dart
+++ mobile_application/lib/widgets/general/image_wrapper.dart
@@ -37,3 +37,17 @@ class ImageWrapper extends StatelessWidget {
           );
   }
 }
+
+extension AssetImages on ImageWrapper {
+  static const String WORK = "job_128.png";
+  static const String EDUCATION = "degree_128.png";
+
+  static const String USER = "user.png";
+  static const String BACK_ARROW = "back_arrow.png";
+  static const String MESSAGE = "message.png";
+  static const String SETTINGS = "settings.png";
+
+  static String socialAssets(String social) => social + ".png";
+
+  static const String DELETE = "ic_delete.png";
+}
