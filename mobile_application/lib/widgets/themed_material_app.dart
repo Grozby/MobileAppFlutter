a1c845701a8849cc96475662def8b6685e7d5ac1
Unknown <gabriele.aldeghi.27@gmail.com>
2020-01-21 16:32:52 +0100

    Corrected status bar overlay with transparent layout

--- mobile_application/lib/widgets/themed_material_app.dart
+++ mobile_application/lib/widgets/themed_material_app.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_application/models/exceptions/no_internet_exception.dart';
-import 'package:mobile_application/widgets/general/loading_error.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
+import '../models/exceptions/no_internet_exception.dart';
 import '../models/exceptions/something_went_wrong_exception.dart';
 import '../providers/authentication/authentication_provider.dart';
 import '../providers/theming/theme_provider.dart';
 import '../screens/homepage_screen.dart';
+import '../screens/initialization_screen.dart';
 import '../screens/landing_screen.dart';
 import '../screens/login_screen.dart';
 import '../screens/messages_screen.dart';
@@ -15,6 +16,7 @@ import '../screens/sign_up_screens/sign_up_choice_screen.dart';
 import '../screens/sign_up_screens/sign_up_screen.dart';
 import '../screens/user_profile_screen.dart';
 import '../screens/waiting_screen.dart';
+import '../widgets/general/loading_error.dart';
 import 'general/custom_alert_dialog.dart';
 
 class ThemedMaterialApp extends StatefulWidget {
@@ -31,58 +33,62 @@ class _ThemedMaterialAppState extends State<ThemedMaterialApp> {
     return MaterialApp(
       theme: currentTheme.getTheme(),
       debugShowCheckedModeBanner: false,
-      home: FutureBuilder(
-        future: authenticationProvider.checkAuthentication() ?? null,
-        builder: (BuildContext ctx, AsyncSnapshot snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return const WaitingScreen();
+      home: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.dark,
+        child: FutureBuilder(
+          future: authenticationProvider.checkAuthentication() ?? null,
+          builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return const WaitingScreen();
 
-            default:
-              if (snapshot.hasError) {
-                if (snapshot.error is NoInternetException) {
-                  return Scaffold(
-                    body: SafeArea(
-                      child: LoadingError(
-                        exception: snapshot.error,
-                        retry: () => setState(() {}),
-                        buildContext: context,
+              default:
+                if (snapshot.hasError) {
+                  if (snapshot.error is NoInternetException) {
+                    return Scaffold(
+                      body: SafeArea(
+                        child: LoadingError(
+                          exception: snapshot.error,
+                          retry: () => setState(() {}),
+                          buildContext: context,
+                        ),
                       ),
-                    ),
-                  );
+                    );
+                  } else {
+                    Future.delayed(
+                      Duration.zero,
+                      () => showErrorDialog(
+                        ctx,
+                        snapshot.error is SomethingWentWrongException
+                            ? (snapshot.error as SomethingWentWrongException)
+                                .text
+                            : "Got some error.",
+                      ),
+                    );
+                    return LandingScreen();
+                  }
+                }
+
+                if (snapshot.data) {
+                  //If true is returned, we are logged in
+                  return const HomepageScreen();
                 } else {
-                  Future.delayed(
-                    Duration.zero,
-                    () => showErrorDialog(
-                      ctx,
-                      snapshot.error is SomethingWentWrongException
-                          ? (snapshot.error as SomethingWentWrongException).text
-                          : "Got some error.",
-                    ),
-                  );
+                  //Otherwise, we show the sign-up screen
+                  if (authenticationProvider.wasLogged) {
+                    Future.delayed(
+                      Duration.zero,
+                      () => showErrorDialog(
+                        ctx,
+                        "You have been logged out. Log in again.",
+                      ),
+                    );
+                  }
+
                   return LandingScreen();
                 }
-              }
-
-              if (snapshot.data) {
-                //If true is returned, we are logged in
-                return const HomepageScreen();
-              } else {
-                //Otherwise, we show the sign-up screen
-                if(authenticationProvider.wasLogged){
-                  Future.delayed(
-                    Duration.zero,
-                        () => showErrorDialog(
-                      ctx,
-                      "You have logged out. Log in again.",
-                    ),
-                  );
-                }
-
-                return LandingScreen();
-              }
-          }
-        },
+            }
+          },
+        ),
       ),
       routes: {
         LandingScreen.routeName: (_) => LandingScreen(),
@@ -93,6 +99,7 @@ class _ThemedMaterialAppState extends State<ThemedMaterialApp> {
         SignUpScreen.routeName: (_) => SignUpScreen(),
         UserProfileScreen.routeName: (ctx) =>
             UserProfileScreen(ModalRoute.of(ctx).settings.arguments),
+        InitializationScreen.routeName: (_) => InitializationScreen(),
         MessagesScreen.routeName: (_) => MessagesScreen(),
       },
     );
