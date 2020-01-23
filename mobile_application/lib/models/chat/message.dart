b13afe4019aa7bcb945e9465106f6d5fd706b591
Unknown <gabriele.aldeghi.27@gmail.com>
2020-01-23 19:44:19 +0100

    Adding model for messages

---
+++ mobile_application/lib/models/chat/message.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'message.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class Message {
+  String userId;
+  String content;
+  String kind;
+  @JsonKey(fromJson: getDateTimeFromString)
+  DateTime createdAt;
+
+  Message({
+    @required this.userId,
+    @required this.content,
+    @required this.kind,
+    @required this.createdAt,
+  });
+
+  ///
+  /// Serializable methods
+  ///
+  factory Message.fromJson(Map<String, dynamic> json) =>
+      _$MessageFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MessageToJson(this);
+
+  static DateTime getDateTimeFromString(String string) {
+    return string == null ? null : DateTime.parse(string);
+  }
+}
---
+++ mobile_application/lib/models/chat/message.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'message.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Message _$MessageFromJson(Map json) {
+  return Message(
+    userId: json['userId'] as String,
+    content: json['content'] as String,
+    kind: json['kind'] as String,
+    createdAt: Message.getDateTimeFromString(json['createdAt'] as String),
+  );
+}
+
+Map<String, dynamic> _$MessageToJson(Message instance) => <String, dynamic>{
+      'userId': instance.userId,
+      'content': instance.content,
+      'kind': instance.kind,
+      'createdAt': instance.createdAt?.toIso8601String(),
+    };
